63bb8a0b779ef74a153deb59374926c436fd7619
JIN Eric <eric.jin@ahi33.org>
2022-08-19 16:35:55 +0200

    19082022

--- js/script.js
+++ js/script.js
@@ -39,6 +39,34 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
   document.querySelector('#search-form').classList.remove('active');
 }
+const onglets = document.querySelectorAll('.onglets');
+const contenu = document.querySelectorAll('.contenu')
+let index = 0;
+onglets.forEach(onglet => {
+
+  onglet.addEventListener('click',() => {
+
+    if(onglet.classList.contains('active')){
+      return;
+    }else {
+      onglet.classList.add('active');
+    }
+    index = onglet.getAttribute('data-anim');
+    console.log(index);
+    for(i = 0; i< onglets.length; i++){
+      if(onglets[i].getAttribute('data-anim') != index){
+        onglets[i].classList.remove('active');
+      }
+    }
+    for(j=0; j<contenu.length; j++){
+      if(contenu[j].getAttribute('data-anim') == index){
+        contenu[j].classList.add('activeContenu');
+      } else {
+        contenu[j].classList.remove('activeContenu');
+      }
+    }
+  })
+})
 
 var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
